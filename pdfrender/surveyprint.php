f16e6a531d23842a60c4bded8521c8d54beffa25
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-29 21:07:49 +0200

    Update surveyprint.php

--- pdfrender/surveyprint.php
+++ pdfrender/surveyprint.php
@@ -3,6 +3,7 @@ require('fpdf.php');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 
 class PdfEncode extends FPDF{

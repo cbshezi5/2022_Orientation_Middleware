706f4de3bab5c487c6114b30a852f04f6cbb6f27
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-29 23:47:05 +0200

    Update surveyprint.php

--- pdfrender/surveyprint.php
+++ pdfrender/surveyprint.php
@@ -44,14 +44,15 @@ class PdfEncode extends FPDF{
             $this->SetTextColor(0, 0, 0);
 
             $this->SetFont('Arial','B',7);
-        for($i = 0; $i < sizeof($data->Survey); $i++)
-        { 
+	    $arr_length = count($data->Survey);
+            for($i = 0; $i < $arr_length; $i++)
+            { 
 	   
 //             $this->SetX($x);
-            $this->Cell(180,6,$data->Survey[$i]->question, 1,0);
+               $this->Cell(180,6,$data->Survey[$i]->question, 1,0);
             
-            $this->Cell(90,6,$data->Survey[$i]->answer, 1,1);
-        }
+               $this->Cell(90,6,$data->Survey[$i]->answer, 1,1);
+            }
 
         
 
